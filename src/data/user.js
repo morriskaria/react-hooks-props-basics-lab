8a28738b910c42a946200fe0ea6db25a79651ef6
Mourice karia <140966785+morriskaria@users.noreply.github.com>
2025-07-12 08:34:50 +0300

    this is my commit

--- src/data/user.js
+++ src/data/user.js
@@ -1,12 +1,13 @@
+// src/data/user.js
 const user = {
   name: "Liza",
   city: "New York",
-  bio: "I made this!",
   color: "firebrick",
+  bio: "I made this!",
   links: {
     github: "https://github.com/liza",
-    linkedin: "https://www.linkedin.com/in/liza/",
-  },
+    linkedin: "https://linkedin.com/in/liza"
+  }
 };
 
 export default user;
